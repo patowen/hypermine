43eb8bfd47695dd170a0d5efb512208bc11f16d9
Patrick Owen <patowen95@gmail.com>
2023-02-25 14:03:40 -0500

    Move VoxelAABB to chunk_collision

--- common/src/graph_collision.rs
+++ common/src/graph_collision.rs
@@ -6,9 +6,8 @@ use crate::{
     chunk_collision::chunk_sphere_cast,
     dodeca::{self, Vertex},
     math,
-    node::{Chunk, ChunkId, ChunkLayout, DualGraph, VoxelData},
+    node::{Chunk, ChunkId, ChunkLayout, DualGraph},
     proto::Position,
-    world::Material,
 };
 
 /// Performs sphere casting (swept collision query) against the voxels in the `DualGraph`
@@ -133,35 +132,6 @@ pub fn sphere_cast(
     Ok(endpoint)
 }
 
-/// Contains all the immutable data needed for `chunk_sphere_cast` to perform its logic
-pub struct ChunkSphereCastContext<'a> {
-    pub dimension: usize,
-    pub dual_to_grid_factor: f32,
-    pub chunk: ChunkId,
-    pub transform: na::Matrix4<f32>,
-    pub voxel_data: &'a VoxelData,
-    pub collider_radius: f32,
-    pub ray: &'a Ray,
-    pub bounding_box: VoxelAABB,
-}
-
-impl ChunkSphereCastContext<'_> {
-    /// Convenience function to get data from a single voxel given its coordinates.
-    /// Each coordinate runs from `0` to `dimension + 1` inclusive, as margins are included.
-    /// To get data within the chunk, use coordinates in the range from `1` to `dimension` inclusive.
-    pub fn get_voxel(&self, coords: [usize; 3]) -> Material {
-        let dimension_with_margin = self.dimension + 2;
-        debug_assert!(coords[0] < dimension_with_margin);
-        debug_assert!(coords[1] < dimension_with_margin);
-        debug_assert!(coords[2] < dimension_with_margin);
-        self.voxel_data.get(
-            coords[0]
-                + coords[1] * dimension_with_margin
-                + coords[2] * dimension_with_margin.pow(2),
-        )
-    }
-}
-
 /// Where a cast ray ended, and all information about the hit at the end if such a hit occurred.
 #[derive(Debug)]
 pub struct CastEndpoint {
@@ -189,23 +159,6 @@ pub struct CastHit {
     /// of the sphere casting. To get the actual normal vector, project it so that it is orthogonal
     /// to the endpoint in Lorentz space.
     pub normal: na::Vector4<f32>,
-}
-
-impl CastEndpoint {
-    /// Convenience function to report a new hit found when sphere casting. The `normal` parameter
-    /// should be provided in the chunk's "dual" coordinate system.
-    pub fn update(
-        &mut self,
-        context: &ChunkSphereCastContext<'_>,
-        tanh_distance: f32,
-        normal: na::Vector4<f32>,
-    ) {
-        self.tanh_distance = tanh_distance;
-        self.hit = Some(CastHit {
-            chunk: context.chunk,
-            normal: context.transform * normal,
-        });
-    }
 }
 
 /// A ray in hyperbolic space. The fields must be lorentz normalized, with `mip(position, position) == -1`,
@@ -243,90 +196,15 @@ impl std::ops::Mul<&Ray> for na::Matrix4<f32> {
     }
 }
 
-/// Represents a discretized region in the voxel grid contained by an axis-aligned bounding box.
-pub struct VoxelAABB {
-    // The bounds are of the form [[x_min, x_max], [y_min, y_max], [z_min, z_max]], using voxel coordinates with margins.
-    // Any voxel that intersects the cube of interest is included in these bounds. By adding or subtracting 1 in the right
-    // places, these bounds can be used to find other useful info related to the cube of interset, such as what grid points
-    // it contains.
-    bounds: [[usize; 2]; 3],
-}
-
-impl VoxelAABB {
-    /// Returns a bounding box that is guaranteed to cover a given radius around a ray segment. Returns None if the
-    /// bounding box lies entirely outside the chunk, including its margins.
-    pub fn from_ray_segment_and_radius(
-        layout: &ChunkLayout,
-        ray: &Ray,
-        tanh_distance: f32,
-        radius: f32,
-    ) -> Option<VoxelAABB> {
-        // Convert the ray to grid coordinates
-        let grid_start =
-            na::Point3::from_homogeneous(ray.position).unwrap() * layout.dual_to_grid_factor();
-        let grid_end = na::Point3::from_homogeneous(ray.ray_point(tanh_distance)).unwrap()
-            * layout.dual_to_grid_factor();
-        // Convert the radius to grid coordinates using a crude conservative estimate
-        let max_grid_radius = radius * layout.dual_to_grid_factor();
-        let mut bounds = [[0; 2]; 3];
-        for axis in 0..3 {
-            let grid_min = grid_start[axis].min(grid_end[axis]) - max_grid_radius;
-            let grid_max = grid_start[axis].max(grid_end[axis]) + max_grid_radius;
-            let voxel_min = (grid_min + 1.0).floor().max(0.0);
-            let voxel_max = (grid_max + 1.0)
-                .floor()
-                .min(layout.dimension() as f32 + 1.0);
-
-            // This will happen when voxel_min is greater than dimension+1 or voxel_max is less than 0, which
-            // occurs when the cube is out of range.
-            if voxel_min > voxel_max {
-                return None;
-            }
-
-            // We convert to usize here instead of earlier because out-of-range voxel coordinates can be negative.
-            bounds[axis] = [voxel_min.floor() as usize, voxel_max.floor() as usize];
-        }
-
-        Some(VoxelAABB { bounds })
-    }
-
-    /// Iterator over grid points contained in the region, represented as ordered triples
-    pub fn grid_points(
-        &self,
-        axis0: usize,
-        axis1: usize,
-        axis2: usize,
-    ) -> impl Iterator<Item = (usize, usize, usize)> {
-        let bounds = self.bounds;
-        (bounds[axis0][0]..bounds[axis0][1]).flat_map(move |i| {
-            (bounds[axis1][0]..bounds[axis1][1])
-                .flat_map(move |j| (bounds[axis2][0]..bounds[axis2][1]).map(move |k| (i, j, k)))
-        })
-    }
-
-    /// Iterator over grid lines intersecting the region, represented as ordered pairs determining the line's two fixed coordinates
-    pub fn grid_lines(&self, axis0: usize, axis1: usize) -> impl Iterator<Item = (usize, usize)> {
-        let bounds = self.bounds;
-        (bounds[axis0][0]..bounds[axis0][1])
-            .flat_map(move |i| (bounds[axis1][0]..bounds[axis1][1]).map(move |j| (i, j)))
-    }
-
-    /// Iterator over grid planes intersecting the region, represented as integers determining the plane's fixed coordinate
-    pub fn grid_planes(&self, axis: usize) -> impl Iterator<Item = usize> {
-        self.bounds[axis][0]..self.bounds[axis][1]
-    }
-}
-
 #[cfg(test)]
 mod tests {
-    use std::collections::HashSet;
-
     use crate::{
         dodeca::{Side, Vertex},
         graph::NodeId,
-        node::populate_fresh_nodes,
+        node::{populate_fresh_nodes, VoxelData},
         proto::Position,
         traversal::{ensure_nearby, nearby_nodes},
+        world::Material,
     };
 
     use super::*;
@@ -607,135 +485,5 @@ mod tests {
         );
 
         assert!(endpoint.is_ok());
-    }
-
-    /// Any voxel AABB should at least cover a capsule-shaped region consisting of all points
-    /// `radius` units away from the ray's line segment. This region consists of two spheres
-    /// and a cylinder. We only test planes because covered lines and points are a strict subset.
-    #[test]
-    fn voxel_aabb_coverage() {
-        let dimension = 12;
-        let dual_to_grid_factor = Vertex::dual_to_chunk_factor() as f32 * dimension as f32;
-
-        // Pick an arbitrary ray by transforming the positive-x-axis ray.
-        let ray = na::Rotation3::from_axis_angle(&na::Vector3::z_axis(), 0.4).to_homogeneous()
-            * math::translate_along(&na::Vector3::new(0.2, 0.3, 0.1))
-            * &Ray::new(na::Vector4::w(), na::Vector4::x());
-
-        let tanh_distance = 0.2;
-        let radius = 0.1;
-
-        // We want to test that the whole capsule-shaped region around the ray segment is covered by
-        // the AABB. However, the math to test for this is complicated, so we instead check a bunch of
-        // spheres along this ray segment.
-        let num_ray_test_points = 20;
-        let ray_test_points: Vec<_> = (0..num_ray_test_points)
-            .map(|i| {
-                math::lorentz_normalize(
-                    &ray.ray_point(tanh_distance * (i as f32 / (num_ray_test_points - 1) as f32)),
-                )
-            })
-            .collect();
-
-        let aabb = VoxelAABB::from_ray_segment_and_radius(
-            &ChunkLayout::new(dimension),
-            &ray,
-            tanh_distance,
-            radius,
-        )
-        .unwrap();
-
-        // For variable names and further comments, we use a tuv coordinate system, which
-        // is a permuted xyz coordinate system.
-
-        // Test planes in all 3 axes.
-        for t_axis in 0..3 {
-            let covered_planes: HashSet<_> = aabb.grid_planes(t_axis).collect();
-
-            // Check that all uv-aligned planes that should be covered are covered
-            for t in 0..=dimension {
-                if covered_planes.contains(&t) {
-                    continue;
-                }
-
-                let mut plane_normal = na::Vector4::zeros();
-                plane_normal[t_axis] = 1.0;
-                plane_normal[3] = t as f32 / dual_to_grid_factor;
-                let plane_normal = math::lorentz_normalize(&plane_normal);
-
-                for test_point in &ray_test_points {
-                    assert!(
-                        math::mip(test_point, &plane_normal).abs() > radius.sinh(),
-                        "Plane not covered: t_axis={t_axis}, t={t}, test_point={test_point:?}",
-                    );
-                }
-            }
-        }
-
-        // Test lines in all 3 axes
-        for t_axis in 0..3 {
-            let u_axis = (t_axis + 1) % 3;
-            let v_axis = (u_axis + 1) % 3;
-            let covered_lines: HashSet<_> = aabb.grid_lines(u_axis, v_axis).collect();
-
-            // For a given axis, all lines have the same direction, so set up the appropriate vector
-            // in advance.
-            let mut line_direction = na::Vector4::zeros();
-            line_direction[t_axis] = 1.0;
-            let line_direction = line_direction;
-
-            // Check that all t-aligned lines that should be covered are covered
-            for u in 0..=dimension {
-                for v in 0..=dimension {
-                    if covered_lines.contains(&(u, v)) {
-                        continue;
-                    }
-
-                    let mut line_position = na::Vector4::zeros();
-                    line_position[u_axis] = u as f32 / dual_to_grid_factor;
-                    line_position[v_axis] = v as f32 / dual_to_grid_factor;
-                    line_position[3] = 1.0;
-                    let line_position = math::lorentz_normalize(&line_position);
-
-                    for test_point in &ray_test_points {
-                        assert!(
-                            (math::mip(test_point, &line_position).powi(2)
-                                - math::mip(test_point, &line_direction).powi(2))
-                            .sqrt()
-                                > radius.cosh(),
-                            "Line not covered: t_axis={t_axis}, u={u}, v={v}, test_point={test_point:?}",
-                        );
-                    }
-                }
-            }
-        }
-
-        // Test points
-        let covered_points: HashSet<_> = aabb.grid_points(0, 1, 2).collect();
-
-        // Check that all points that should be covered are covered
-        for x in 0..=dimension {
-            for y in 0..=dimension {
-                for z in 0..=dimension {
-                    if covered_points.contains(&(x, y, z)) {
-                        continue;
-                    }
-
-                    let point_position = math::lorentz_normalize(&na::Vector4::new(
-                        x as f32 / dual_to_grid_factor,
-                        y as f32 / dual_to_grid_factor,
-                        z as f32 / dual_to_grid_factor,
-                        1.0,
-                    ));
-
-                    for test_point in &ray_test_points {
-                        assert!(
-                            -math::mip(test_point, &point_position) > radius.cosh(),
-                            "Point not covered: x={x}, y={y}, z={z}, test_point={test_point:?}",
-                        );
-                    }
-                }
-            }
-        }
     }
 }
