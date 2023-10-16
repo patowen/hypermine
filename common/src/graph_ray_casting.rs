4051724d1c431d65b217823f5e727374755cc6d2
Patrick Owen <patowen95@gmail.com>
2023-10-16 14:54:55 -0400

    Use RayTraversal for graph_ray_casting

--- common/src/graph_ray_casting.rs
+++ common/src/graph_ray_casting.rs
@@ -1,17 +1,12 @@
-use std::collections::VecDeque;
-
-use fxhash::FxHashSet;
-
 use crate::{
     chunk_ray_casting::chunk_ray_cast,
     collision_math::Ray,
-    dodeca::Vertex,
-    math,
     node::{Chunk, ChunkId, ChunkLayout, Coords, DualGraph},
     proto::Position,
+    traversal::RayTraverser,
 };
 
-/// Performs sphere casting (swept collision query) against the voxels in the `DualGraph`
+/// Performs ray casting against the voxels in the `DualGraph`
 ///
 /// The `ray` parameter and any resulting hit normals are given in the local coordinate system of `position`.
 ///
@@ -25,53 +20,31 @@ pub fn ray_cast(
     layout: &ChunkLayout,
     position: &Position,
     ray: &Ray,
-    tanh_distance: f32,
+    mut tanh_distance: f32,
 ) -> Result<Option<GraphCastHit>, RayCastError> {
-    // A collision check is assumed to be a miss until a collision is found.
+    // A ray cast is assumed to be a miss until a collision is found.
     // This `hit` variable gets updated over time before being returned.
     let mut hit: Option<GraphCastHit> = None;
 
-    // Pick the vertex closest to position.local as the vertex of the chunk to use to start collision checking
-    let start_vertex = Vertex::iter()
-        .map(|v| {
-            (
-                v,
-                (v.node_to_dual().cast::<f32>() * position.local * math::origin()).w,
-            )
-        })
-        .min_by(|(_, a), (_, b)| a.partial_cmp(b).unwrap())
-        .unwrap()
-        .0;
-
-    // Start a breadth-first search of the graph's chunks, performing collision checks in each relevant chunk.
-    // The `chunk_queue` contains ordered pairs containing the `ChunkId` and the transformation needed to switch
-    // from the original node coordinates to the current chunk's node coordinates.
-    let mut visited_chunks = FxHashSet::<ChunkId>::default();
-    let mut chunk_queue: VecDeque<(ChunkId, na::Matrix4<f32>)> = VecDeque::new();
-    chunk_queue.push_back((ChunkId::new(position.node, start_vertex), position.local));
-
-    // Precalculate the chunk boundaries for collision purposes. If the collider goes outside these bounds,
-    // the corresponding neighboring chunk will also be used for collision checking.
-    let klein_lower_boundary = 0.0;
-    let klein_upper_boundary = Vertex::chunk_to_dual_factor() as f32;
-
-    // Breadth-first search loop
-    while let Some((chunk, node_transform)) = chunk_queue.pop_front() {
+    let mut traverser = RayTraverser::new(graph, *position, ray, 0.0);
+    while let Some((chunk, transform)) = traverser.next(tanh_distance) {
+        let Some(chunk) = chunk else {
+            // Ray reached chunk outside of graph
+            return Err(RayCastError::OutOfBounds);
+        };
         let Chunk::Populated {
             voxels: ref voxel_data,
             ..
         } = graph[chunk]
         else {
-            // Collision checking on unpopulated chunk
+            // Ray reached unpopulated chunk
             return Err(RayCastError::OutOfBounds);
         };
-        let local_ray = chunk.vertex.node_to_dual().cast::<f32>() * node_transform * ray;
 
-        // Check collision within a single chunk
-        let current_tanh_distance = hit.as_ref().map_or(tanh_distance, |hit| hit.tanh_distance);
-        hit = chunk_ray_cast(voxel_data, layout, &local_ray, current_tanh_distance).map_or(
+        hit = chunk_ray_cast(voxel_data, layout, &(transform * ray), tanh_distance).map_or(
             hit,
             |hit| {
+                tanh_distance = hit.tanh_distance;
                 Some(GraphCastHit {
                     tanh_distance: hit.tanh_distance,
                     chunk,
@@ -81,45 +54,6 @@ pub fn ray_cast(
                 })
             },
         );
-
-        // Compute the Klein-Beltrami coordinates of the ray segment's endpoints. To check whether neighboring chunks
-        // are needed, we need to check whether the endpoints of the line segments lie outside the boundaries of the square
-        // bounded by `klein_lower_boundary` and `klein_upper_boundary`.
-        let klein_ray_start = na::Point3::from_homogeneous(local_ray.position).unwrap();
-        let klein_ray_end =
-            na::Point3::from_homogeneous(local_ray.ray_point(current_tanh_distance)).unwrap();
-
-        // Add neighboring chunks as necessary based on a conservative AABB check, using one coordinate at a time.
-        for axis in 0..3 {
-            // Check for neighboring nodes
-            if klein_ray_start[axis] <= klein_lower_boundary
-                || klein_ray_end[axis] <= klein_lower_boundary
-            {
-                let side = chunk.vertex.canonical_sides()[axis];
-                let next_node_transform = side.reflection().cast::<f32>() * node_transform;
-                // If we have to do collision checking on nodes that don't exist in the graph, we cannot have a conclusive result.
-                let Some(neighbor) = graph.neighbor(chunk.node, side) else {
-                    // Collision checking on nonexistent node
-                    return Err(RayCastError::OutOfBounds);
-                };
-                // Assuming everything goes well, add the new chunk to the queue.
-                let next_chunk = ChunkId::new(neighbor, chunk.vertex);
-                if visited_chunks.insert(next_chunk) {
-                    chunk_queue.push_back((next_chunk, next_node_transform));
-                }
-            }
-
-            // Check for neighboring chunks within the same node
-            if klein_ray_start[axis] >= klein_upper_boundary
-                || klein_ray_end[axis] >= klein_upper_boundary
-            {
-                let vertex = chunk.vertex.adjacent_vertices()[axis];
-                let next_chunk = ChunkId::new(chunk.node, vertex);
-                if visited_chunks.insert(next_chunk) {
-                    chunk_queue.push_back((next_chunk, node_transform));
-                }
-            }
-        }
     }
 
     Ok(hit)
