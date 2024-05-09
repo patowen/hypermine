27a1447d4175882258501748447f2054f6b9c40d
Patrick Owen <patowen95@gmail.com>
2024-05-09 00:06:30 -0400

    Draft widget for rendering material icons

--- client/src/graphics/gui.rs
+++ client/src/graphics/gui.rs
@@ -1,5 +1,9 @@
 use yakui::{
-    align, colored_box, colored_box_container, label, pad, widgets::Pad, Alignment, Color,
+    align, colored_box, colored_box_container, label, pad,
+    paint::{PaintMesh, Vertex},
+    widget::{LayoutContext, PaintContext, Widget},
+    widgets::Pad,
+    Alignment, Color, Constraints, Rect, TextureId, Vec2,
 };
 
 use crate::Sim;
@@ -36,5 +40,66 @@ impl GuiState {
                 });
             });
         });
+    }
+}
+
+#[derive(Debug)]
+struct ItemIconWidget(Option<TextureId>);
+
+impl ItemIconWidget {
+    const CUBE_VERTICES: [([f32; 2], [f32; 2], f32); 12] = {
+        let r3 = 1.7320508f32; // Square root of 3
+        [
+            ([0.0, -2.0], [0.0, 0.0], 1.0),
+            ([-r3, -1.0], [0.0, 1.0], 1.0),
+            ([0.0, 0.0], [1.0, 1.0], 1.0),
+            ([r3, -1.0], [1.0, 0.0], 1.0),
+            ([-r3, -1.0], [0.0, 0.0], 0.9),
+            ([-r3, 1.0], [0.0, 1.0], 0.9),
+            ([0.0, 2.0], [1.0, 1.0], 0.9),
+            ([0.0, 0.0], [1.0, 0.0], 0.9),
+            ([0.0, 0.0], [0.0, 0.0], 0.8),
+            ([0.0, 2.0], [0.0, 1.0], 0.8),
+            ([r3, 1.0], [1.0, 1.0], 0.8),
+            ([r3, -1.0], [1.0, 0.0], 0.8),
+        ]
+    };
+
+    const CUBE_INDICES: [u16; 18] = [0, 1, 2, 3, 0, 2, 4, 5, 6, 7, 4, 6, 8, 9, 10, 11, 8, 10];
+}
+
+impl Widget for ItemIconWidget {
+    type Props<'a> = Option<TextureId>;
+
+    type Response = ();
+
+    fn new() -> Self {
+        Self(None)
+    }
+
+    fn update(&mut self, props: Self::Props<'_>) -> Self::Response {
+        self.0 = props;
+    }
+
+    fn layout(&self, _ctx: LayoutContext<'_>, input: Constraints) -> Vec2 {
+        input.constrain_min(Vec2::splat(200.0))
+    }
+
+    fn paint(&self, ctx: PaintContext<'_>) {
+        let layout_node = ctx.layout.get(ctx.dom.current()).unwrap();
+
+        if let Some(texture) = self.0 {
+            let vertices = Self::CUBE_VERTICES.map(|(p, t, c)| {
+                Vertex::new(
+                    Vec2::new(p[0] + 2.0, p[1] + 2.0) * layout_node.rect.size() / 4.0
+                        + layout_node.rect.pos(),
+                    t,
+                    [c, c, c, 1.0],
+                )
+            });
+            let mut hexagon = PaintMesh::new(vertices, Self::CUBE_INDICES);
+            hexagon.texture = Some((texture, Rect::ONE));
+            ctx.paint.add_mesh(hexagon);
+        }
     }
 }
