280e8a500dbbe26fff17d775dacff2aeda0589ee
Patrick Owen <patowen95@gmail.com>
2024-05-11 21:20:29 -0400

    Add client logic for dealing with the inventory

--- client/src/sim.rs
+++ client/src/sim.rs
@@ -15,7 +15,8 @@ use common::{
     graph_ray_casting,
     node::{populate_fresh_nodes, ChunkId, VoxelData},
     proto::{
-        self, BlockUpdate, Character, CharacterInput, CharacterState, Command, Component, Position,
+        self, BlockUpdate, Character, CharacterInput, CharacterState, Command, Component, Item,
+        Position,
     },
     sanitize_motion_input,
     world::Material,
@@ -346,6 +347,25 @@ impl Sim {
             };
             self.graph.populate_chunk(chunk_id, voxel_data);
         }
+        for (subject, new_item) in msg.inventory_additions {
+            self.world
+                .get::<&mut Character>(*self.entity_ids.get(&subject).unwrap())
+                .unwrap()
+                .inventory
+                .push(new_item);
+            println!("New item: {}", new_item);
+        }
+        for (subject, removed_item) in msg.inventory_removals {
+            let inventory = &mut self
+                .world
+                .get::<&mut Character>(*self.entity_ids.get(&subject).unwrap())
+                .unwrap()
+                .inventory;
+
+            let position = inventory.iter().position(|&id| id == removed_item).unwrap();
+            inventory.swap_remove(position);
+            println!("Removed item: {}", removed_item);
+        }
     }
 
     fn spawn(
@@ -512,8 +532,19 @@ impl Sim {
             (hit.chunk, hit.voxel_coords)
         };
 
+        let mut consumed_item = None;
         let material = if placing {
-            self.selected_material
+            let &consumed_item_id = self
+                .world
+                .get::<&Character>(self.local_character.unwrap())
+                .unwrap()
+                .inventory
+                .first()?;
+            consumed_item = Some(consumed_item_id);
+            self.world
+                .get::<&Item>(*self.entity_ids.get(&consumed_item_id).unwrap())
+                .unwrap()
+                .material
         } else {
             Material::Void
         };
@@ -522,7 +553,7 @@ impl Sim {
             chunk_id: block_pos.0,
             coords: block_pos.1,
             new_material: material,
-            consumed_item: None,
+            consumed_item,
         })
     }
 }
