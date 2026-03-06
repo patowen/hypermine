d54d00fddf686e9afa2b6ec60bb8ec96349e4c87
Patrick Owen <patowen95@gmail.com>
2026-03-05 22:02:10 -0500

    Fix silly error when comparing both binary-like tiling formulas

--- client/src/binary_like_tiling.rs
+++ client/src/binary_like_tiling.rs
@@ -10,26 +10,31 @@ fn voxel_to_mvector_boosted(voxel: na::Vector3<f32>, boost: f32) -> MVector<f32>
     let f = 1.0 - sqrtf(1.0 - sqr(voxel.x) - sqr(voxel.y));
     let g = f * sqr(coshf(boost));
     let u = voxel.z + boost;
-    println!("{}", g);
     MVector::new(
         voxel.x * coshf(boost),
         voxel.y * coshf(boost),
-        sqr(coshf(boost)) * tanhf(-boost + u) + coshf(boost) * sinhf(boost),
-        //(coshf(u) * tanhf(boost) * g + sinhf(u) * (1.0 - g)) / (coshf(u) - tanhf(boost) * sinhf(u)),
+        (coshf(u) * tanhf(boost) * g + sinhf(u) * (1.0 - g)) / (coshf(u) - tanhf(boost) * sinhf(u)),
         0.0, // TODO
     )
 }
 
 #[cfg(test)]
 mod tests {
+    use common::math::MIsometry;
+
     use super::*;
 
     #[test]
     fn example() {
-        let example = na::vector![0.0, 0.0, 2.0];
+        let example = na::vector![0.1, 0.2, 2.0];
         let boost = 1.0;
         // I'll have to think about this later.
-        println!("{:?}", voxel_to_mvector_simple(example) * coshf(boost));
+        println!(
+            "{:?}",
+            MIsometry::translation_along(&(na::Vector3::z() * boost))
+                * voxel_to_mvector_simple(example)
+                * coshf(boost)
+        );
         println!("{:?}", voxel_to_mvector_boosted(example, boost));
     }
 }
