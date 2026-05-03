e07ace8bf2f096ef9d7633f8fbe9ad44056628d9
Patrick Owen <patowen95@gmail.com>
2026-05-03 17:48:01 -0400

    Add license of offset-allocator

--- client/src/offset_allocator/mod.rs
+++ client/src/offset_allocator/mod.rs
@@ -2,6 +2,32 @@
 //! updated for improved readability and optimized for Hypermine's use case.
 //! It is based on commit 9449d4d81be91396938b05894fad443f63093077.
 
+/* The license notice of the original offset-allocator crate this came from is as follows:
+
+MIT License
+
+Copyright (c) 2023 Sebastian Aaltonen, Patrick Walton
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+*/
+
 mod bins_map;
 mod node_index;
 mod small_float;
