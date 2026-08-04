588d506ef53b27730635e3302fb017f8f13056b4
Patrick Owen <patowen95@gmail.com>
2026-08-04 00:03:02 -0400

    Remove poor attempt at scoping threads in asset_loader

--- client/src/asset_loader.rs
+++ client/src/asset_loader.rs
@@ -1,10 +1,10 @@
 use std::{
-    sync::{Arc, mpsc},
+    sync::Arc,
     thread::{self, JoinHandle},
 };
 
 use ash::vk;
-use lahar::ParallelQueue;
+use lahar::{ParallelQueue, parallel_queue};
 use skid_steer::Context;
 
 use crate::{Config, graphics::Base, growable_ring::GrowableRing};
@@ -76,109 +76,81 @@ impl HelperSemaphore {
 
 pub struct AssetLoader {
     gfx: Arc<Base>,
-    join_handle: Option<JoinHandle<()>>,
-    cancellation_send: mpsc::Sender<()>, // TODO: Use CancellationToken
+    cancellation_send: std::sync::mpsc::Sender<()>, // TODO: Use CancellationToken
     queue_unpark_semaphore: HelperSemaphore,
     loader: skid_steer::Loader,
+    task_executor_threads: Vec<JoinHandle<()>>,
+    parallel_queue_driver_thread: Option<JoinHandle<ParallelQueue>>,
+    staging: Option<Arc<GrowableRing>>,
 }
 
 impl AssetLoader {
     pub fn new(gfx: Arc<Base>, config: Arc<Config>) -> Self {
-        let (cancellation_send, cancellation_receive) = mpsc::channel::<()>();
-        let mut queue =
-            unsafe { ParallelQueue::new(&gfx.device, gfx.queue_family, gfx.queue, None) };
+        let (cancellation_send, cancellation_receive) = std::sync::mpsc::channel::<()>();
+        let queue = unsafe { ParallelQueue::new(&gfx.device, gfx.queue_family, gfx.queue, None) };
         let loader = skid_steer::Loader::new();
-        let mut staging = GrowableRing::new(&gfx, 32 * 1024 * 1024);
+        let staging = Arc::new(GrowableRing::new(&gfx, 32 * 1024 * 1024));
         let queue_unpark_semaphore = unsafe { HelperSemaphore::new(&gfx.device) };
 
-        let join_handle = {
-            let gfx = gfx.clone();
-            let config = config.clone();
+        let parallel_queue_waiter = Arc::new(ParallelQueueWaiter {
+            semaphore: queue.semaphore(),
+            queue_unpark_semaphore,
+            progress_changed: tokio::sync::Notify::new(),
+        });
+
+        // TODO: Use dynamic number of threads
+        let mut task_executor_threads = vec![];
+        for i in 0..2 {
+            let gfx = Arc::clone(&gfx);
+            let config = Arc::clone(&config);
+            let handle = unsafe { queue.handle(&gfx.device) };
+            let staging = Arc::clone(&staging);
+            let parallel_queue_waiter = Arc::clone(&parallel_queue_waiter);
             let loader = loader.clone();
-            thread::Builder::new()
-                .name("asset_loader_driver".to_owned())
+
+            let thread = thread::Builder::new()
+                .name(format!("task_executor_{}", i).to_owned())
                 .spawn(move || {
-                    let parallel_queue_waiter = ParallelQueueWaiter {
-                        semaphore: queue.semaphore(),
+                    run_task_executor(
+                        &gfx,
+                        &config,
+                        handle,
+                        &staging,
+                        &parallel_queue_waiter,
+                        loader.clone(),
+                    );
+                })
+                .unwrap();
+            task_executor_threads.push(thread);
+        }
+
+        let parallel_queue_driver_thread = Some({
+            let gfx = Arc::clone(&gfx);
+            let staging = Arc::clone(&staging);
+
+            thread::Builder::new()
+                .name("parallel_queue_driver".to_owned())
+                .spawn(move || {
+                    run_parallel_queue_driver(
+                        &gfx,
+                        queue,
                         queue_unpark_semaphore,
-                        progress_changed: tokio::sync::Notify::new(),
-                    };
-
-                    thread::scope(|s| {
-                        let loader = &loader;
-                        let staging = &staging;
-                        let parallel_queue_waiter = &parallel_queue_waiter;
-                        // TODO: Use dynamic number of threads
-                        for i in 0..2 {
-                            let mut handle = unsafe { queue.handle(&gfx.device) };
-                            let gfx: &Base = &gfx;
-                            let config: &Config = &config;
-                            thread::Builder::new()
-                                .name(format!("task_executor_{}", i).to_owned())
-                                .spawn_scoped(s, move || {
-                                    let runtime = tokio::runtime::LocalRuntime::new().unwrap();
-                                    // TODO: This is the wrong pattern for spawning async tasks because concurrency is lost.
-                                    runtime.block_on(async {
-                                        while let Some(task) = loader.next_task().await {
-                                            tracing::trace!("Found task");
-                                            let context = Context::from_slice(&[
-                                                gfx,
-                                                config, // TODO: I don't want `Config` in the context long-term
-                                                &handle,
-                                                staging,
-                                                parallel_queue_waiter,
-                                            ]);
-                                            task.run(&context).await;
-                                            tracing::trace!("Asset loaded {}", i);
-                                        }
-                                        tracing::trace!("Ending task executor {}", i);
-                                        unsafe { handle.destroy(&gfx.device) };
-                                    });
-                                })
-                                .unwrap();
-                        }
-
-                        // Driver thread
-                        loop {
-                            // Systems increment the `queue_unpark_semaphore` value when they want to guarantee
-                            // that we don't park unless certain things are done. `ParallelQueueWaiter`
-                            // wants to ensure that `ParallelQueue::drive` is called, while the cleanup code
-                            // wants to ensure `cancellation_recieve` is checked. Therefore, we put these two
-                            // operations between reading and waiting on  `queue_unpark_semaphore`
-                            let queue_unpark_semaphore_next_value =
-                                unsafe { queue_unpark_semaphore.get_next_value(&gfx.device) };
-                            unsafe { queue.drive(&gfx.device) };
-                            if cancellation_receive.try_recv() != Err(mpsc::TryRecvError::Empty) {
-                                break;
-                            }
-                            let semaphore_value = unsafe {
-                                queue.park(
-                                    &gfx.device,
-                                    queue_unpark_semaphore.0,
-                                    queue_unpark_semaphore_next_value,
-                                )
-                            };
-                            parallel_queue_waiter.progress_changed.notify_waiters();
-                            unsafe { staging.tick(&gfx.device, semaphore_value) };
-                        }
-                        tracing::trace!("Cancellation received. Should shut down AssetLoader");
-                        loader.close(); // TODO: Think about how to actually close the loader, such as cancelling in-progress tasks.
-                    });
-                    tracing::trace!("All threads shut down");
-                    unsafe { queue.drain(&gfx.device) };
-                    unsafe { queue.destroy(&gfx.device) };
-                    unsafe { queue_unpark_semaphore.destroy(&gfx.device) };
-                    unsafe { staging.destroy(&gfx.device) };
+                        cancellation_receive,
+                        &parallel_queue_waiter,
+                        &staging,
+                    )
                 })
                 .unwrap()
-        };
+        });
 
         AssetLoader {
             gfx,
-            join_handle: Some(join_handle),
             cancellation_send,
             queue_unpark_semaphore,
             loader,
+            task_executor_threads,
+            parallel_queue_driver_thread,
+            staging: Some(staging),
         }
     }
 
@@ -190,10 +162,96 @@ impl AssetLoader {
     }
 }
 
+fn run_task_executor(
+    gfx: &Base,
+    config: &Config,
+    mut handle: parallel_queue::Handle,
+    staging: &GrowableRing,
+    parallel_queue_waiter: &ParallelQueueWaiter,
+    loader: skid_steer::Loader,
+) {
+    let runtime = tokio::runtime::LocalRuntime::new().unwrap();
+    // TODO: This is the wrong pattern for spawning async tasks because concurrency is lost.
+    runtime.block_on(async {
+        while let Some(task) = loader.next_task().await {
+            tracing::trace!("Found task");
+            let context = Context::from_slice(&[
+                gfx,
+                config, // TODO: I don't want `Config` in the context long-term
+                &handle,
+                staging,
+                parallel_queue_waiter,
+            ]);
+            task.run(&context).await;
+            tracing::trace!(
+                "Asset loaded {}",
+                thread::current().name().unwrap_or("<unnamed>")
+            );
+        }
+        tracing::trace!(
+            "Ending task executor {}",
+            thread::current().name().unwrap_or("<unnamed>")
+        );
+    });
+    unsafe { handle.destroy(&gfx.device) };
+}
+
+fn run_parallel_queue_driver(
+    gfx: &Base,
+    mut queue: ParallelQueue,
+    queue_unpark_semaphore: HelperSemaphore,
+    cancellation_receive: std::sync::mpsc::Receiver<()>,
+    parallel_queue_waiter: &ParallelQueueWaiter,
+    staging: &GrowableRing,
+) -> ParallelQueue {
+    loop {
+        // Systems increment the `queue_unpark_semaphore` value when they want to guarantee
+        // that we don't park unless certain things are done. `ParallelQueueWaiter`
+        // wants to ensure that `ParallelQueue::drive` is called, while the cleanup code
+        // wants to ensure `cancellation_recieve` is checked. Therefore, we put these two
+        // operations between reading and waiting on  `queue_unpark_semaphore`
+        let queue_unpark_semaphore_next_value =
+            unsafe { queue_unpark_semaphore.get_next_value(&gfx.device) };
+        unsafe { queue.drive(&gfx.device) };
+        if cancellation_receive.try_recv() != Err(std::sync::mpsc::TryRecvError::Empty) {
+            break;
+        }
+        let semaphore_value = unsafe {
+            queue.park(
+                &gfx.device,
+                queue_unpark_semaphore.0,
+                queue_unpark_semaphore_next_value,
+            )
+        };
+        parallel_queue_waiter.progress_changed.notify_waiters();
+        unsafe { staging.tick(&gfx.device, semaphore_value) };
+    }
+    queue
+}
+
 impl Drop for AssetLoader {
     fn drop(&mut self) {
         let _ = self.cancellation_send.send(());
         unsafe { self.queue_unpark_semaphore.signal(&self.gfx.device) };
-        self.join_handle.take().unwrap().join().unwrap();
+        let mut queue = self
+            .parallel_queue_driver_thread
+            .take()
+            .unwrap()
+            .join()
+            .unwrap();
+        tracing::trace!("Shutting down down AssetLoader");
+        self.loader.close(); // TODO: Think about how to actually close the loader, such as cancelling in-progress tasks.
+        for task_executor_thread in self.task_executor_threads.drain(..) {
+            task_executor_thread.join().unwrap();
+        }
+        unsafe { queue.drain(&self.gfx.device) };
+        unsafe { queue.destroy(&self.gfx.device) };
+        unsafe { self.queue_unpark_semaphore.destroy(&self.gfx.device) };
+        unsafe {
+            Arc::try_unwrap(self.staging.take().unwrap())
+                .map_err(|_| ())
+                .expect("All threads using staging are now joined")
+                .destroy(&self.gfx.device)
+        };
     }
 }
